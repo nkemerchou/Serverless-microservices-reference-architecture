1de411ce1314b0e4a445fc939665f7ee5d598e57
nkemerchou <nkemerchou@hotmail.com>
2021-02-28 11:45:53 -0500

    Create settings.js

---
+++ web/serverless-microservices-web/public/js/settings.js
@@ -0,0 +1,13 @@
+// Auth
+window.authClientId = '11aaf62a-1b38-483c-9cec-4f28c3c9467a';
+window.authAuthority = 'https://rideshare2021.b2clogin.com/tfp/rideshare2021.onmicrosoft.com/b2c_1_default-signin/v2.0';
+window.authScopes = ['https://rideshare2021.onmicrosoft.com/api/rideshare'];
+window.authEnabled = true;
+
+// API endpoints
+window.apiKey = '3314285c5d1541e784012ef9fda59eab';
+window.apiBaseUrl = 'https://rideshare2021.azure-api.net';
+window.apiDriversBaseUrl = `${window.apiBaseUrl}/d`;
+window.apiTripsBaseUrl = `${window.apiBaseUrl}/t`;
+window.apiPassengersBaseUrl = `${window.apiBaseUrl}/p`;
+window.signalrInfoUrl = 'https://ridesharetripsfunctionapp.azurewebsites.net/api/signalrinfo';
